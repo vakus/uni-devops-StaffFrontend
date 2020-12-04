3e9971240f16bb653773b061e8bea5c756896389
vakus <sysojewosinskidominik@gmail.com>
2020-12-04 18:06:34 +0000

    Implemented remote suppliers proxy

--- StaffFrontend/Proxies/SuplierProxy/SupplierProxyRemote.cs
+++ StaffFrontend/Proxies/SuplierProxy/SupplierProxyRemote.cs
@@ -1,7 +1,9 @@
-﻿using StaffFrontend.Models;
+﻿using Microsoft.Extensions.Configuration;
+using StaffFrontend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace StaffFrontend.Proxies.SuplierProxy
@@ -9,9 +11,39 @@ namespace StaffFrontend.Proxies.SuplierProxy
 
     public class SupplierProxyRemote : ISupplierProxy
     {
-        public Task CreateSupplier(Supplier supplier)
+
+        private IHttpClientFactory _clientFactory;
+        private IConfigurationSection _config;
+        public SupplierProxyRemote(IHttpClientFactory clientFactory, IConfiguration config)
         {
-            throw new NotImplementedException();
+            _clientFactory = clientFactory;
+            _config = config.GetSection("SupplierMicroservice");
+        }
+
+        public async Task CreateSupplier(Supplier supplier)
+        {
+            Dictionary<string, object> values = new Dictionary<string, object>
+            {
+                { "supplier-name", supplier.SupplierName },
+                { "supplier-email", supplier.SupplierEmail },
+                { "supplier-phone", supplier.SupplierContactNumber },
+                { "supplier-address", supplier.SupplierAddress }
+            };
+
+            string url = Utils.createUriBuilder(_config.GetSection("CreateSupplier"), values).ToString();
+
+            var client = _clientFactory.CreateClient();
+
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+
+            var response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                //error occured can not receive information
+                throw new SystemException("Could not receive data from remote service");
+            }
         }
 
         public Task DeleteSupplier(int supplierid)
@@ -19,19 +51,79 @@ namespace StaffFrontend.Proxies.SuplierProxy
             throw new NotImplementedException();
         }
 
-        public Task<Supplier> GetSupplier(int supplierid)
+        public async Task<Supplier> GetSupplier(int supplierid)
         {
-            throw new NotImplementedException();
+            Dictionary<string, object> values = new Dictionary<string, object>
+            {
+                { "supplier-id", supplierid }
+            };
+            string url = Utils.createUriBuilder(_config.GetSection("GetSupplier"), values).ToString();
+
+            var client = _clientFactory.CreateClient();
+
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+
+            var response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                //error occured can not receive information
+                throw new SystemException("Could not receive data from remote service");
+            }
+            else
+            {
+                return await response.Content.ReadAsAsync<Supplier>();
+            }
         }
 
-        public Task<List<Supplier>> GetSuppliers()
+        public async Task<List<Supplier>> GetSuppliers()
         {
-            throw new NotImplementedException();
+            string url = Utils.createUriBuilder(_config.GetSection("GetSuppliers"), new Dictionary<string, object> { }).ToString();
+
+            var client = _clientFactory.CreateClient();
+
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+
+            var response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                //error occured can not receive information
+                throw new SystemException("Could not receive data from remote service");
+            }
+            else
+            {
+                return await response.Content.ReadAsAsync<List<Supplier>>();
+            }
         }
 
-        public Task UpdateSupplier(Supplier supplier)
+        public async Task UpdateSupplier(Supplier supplier)
         {
-            throw new NotImplementedException();
+            Dictionary<string, object> values = new Dictionary<string, object>
+            {
+                { "supplier-id", supplier.SupplierId },
+                { "supplier-name", supplier.SupplierName },
+                { "supplier-email", supplier.SupplierEmail },
+                { "supplier-phone", supplier.SupplierContactNumber },
+                { "supplier-address", supplier.SupplierAddress }
+            };
+
+            string url = Utils.createUriBuilder(_config.GetSection("EditSupplier"), values).ToString();
+
+            var client = _clientFactory.CreateClient();
+
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+
+            var response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                //error occured can not receive information
+                throw new SystemException("Could not receive data from remote service");
+            }
         }
     }
 }
