387fa10eba3256b8015540b62c8841ac8f268e63
vakus <sysojewosinskidominik@gmail.com>
2020-12-07 20:31:09 +0000

    Updated Remote Proxy to list items from remote service, updated Utils to handle Post/Get requests from config

--- StaffFrontend/Utils.cs
+++ StaffFrontend/Utils.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace StaffFrontend
@@ -16,25 +18,26 @@ namespace StaffFrontend
          * Then it will create path which will be ran through processString, with values.
          * After that, this function will check if get parameters are set in the configuration
          * if they are they will be added to the UriBuilder, with parameters being also processed by
-         * processString function
+         * processString function.
          */
-        public static UriBuilder createUriBuilder(IConfigurationSection config, Dictionary<string, object> values)
+        public static UriBuilder CreateUriBuilder(IConfigurationSection config, Dictionary<string, object> values)
         {
-            UriBuilder builder = new UriBuilder(config.GetValue<string>("domain"));
+            UriBuilder builder = new UriBuilder(config.GetValue<string>("domain"))
+            {
+                Port = config.GetValue<int>("port"),
+                Path = ProcessString(config.GetValue<string>("path"), values)
+            };
 
-            builder.Port = config.GetValue<int>("port");
-            builder.Path = processString(config.GetValue<string>("path"), values);
-
-            builder.Query = createUriGetParameters(config, values, builder);
+            builder.Query = CreateGetParameters(config, values, builder);
 
             return builder;
         }
 
         /**
          * This function creates get parameters for UriBuilder, and returns the query as a string.
-         * After processing parameters with values, if the result would be an empty value, it will be skipped instead
+         * After processing parameters with values, if the result would be an empty value, it will be skipped instead.
          */
-        public static string createUriGetParameters(IConfigurationSection config, Dictionary<string, object> values, UriBuilder builder)
+        public static string CreateGetParameters(IConfigurationSection config, Dictionary<string, object> values, UriBuilder builder)
         {
             IConfigurationSection parameters = config.GetSection("get");
             var Query = HttpUtility.ParseQueryString(builder.Query);
@@ -49,7 +52,7 @@ namespace StaffFrontend
                 foreach (KeyValuePair<string, string> entry in entries)
                 {
                     //we want only to add non-empty/non-null parameters
-                    string processedValue = processString(entry.Value, values);
+                    string processedValue = ProcessString(entry.Value, values);
                     if (!String.IsNullOrEmpty(processedValue))
                     {
                         Query[entry.Key] = processedValue;
@@ -58,6 +61,32 @@ namespace StaffFrontend
             }
 
             return Query.ToString();
+        }
+
+        /**
+         * This function creates list of key value pairs that represent post parameters based on config file as well as data from values.
+         * This function works similar to CreateGetParameters.
+         * 
+         * After processing parameters with values, if the result would be an empty value, it will be skipped instead.
+         */
+        public static List<KeyValuePair<string, string>> CreatePostParameters(IConfigurationSection config, Dictionary<string, object> values)
+        {
+            var resp = new List<KeyValuePair<string, string>>();
+            if (config.Exists())
+            {
+                var entries = new Dictionary<string, string>();
+                config.Bind(entries);
+
+                foreach(KeyValuePair<string, string> entry in entries)
+                {
+                    string processedValue = ProcessString(entry.Value, values);
+                    if (!String.IsNullOrEmpty(processedValue))
+                    {
+                        resp.Add(new KeyValuePair<string, string>(entry.Key, processedValue));
+                    }
+                }
+            }
+            return resp;
         }
 
         /**
@@ -72,7 +101,7 @@ namespace StaffFrontend
          * If we have string "{replace}" and list which has key "replace" and value 42, the result will be "42"
          * If we have string "test/replace" and list which has key "replace" and value 42, the result will be "test/replace"
          */
-        public static string processString(string str, Dictionary<string, object> values)
+        public static string ProcessString(string str, Dictionary<string, object> values)
         {
             string processed = str;
             foreach(KeyValuePair<string, object> entry in values)
@@ -91,6 +120,30 @@ namespace StaffFrontend
                 }
             }
             return processed;
+        }
+
+        /**
+         * This function returns response from get/post request based on information in config as well as from values.
+         * If config has "post" section, the call will automatically be switched to "post". All Get parameters will be sent along Post request
+         * If config doesnt have "post" section, the call will automatically be switched to "get". Only Get parameters will be sent with Get request
+         */
+        public static async Task<HttpResponseMessage> Request(HttpClient client, IConfigurationSection config, Dictionary<string, object> values)
+        {
+            string url = Utils.CreateUriBuilder(config, values).ToString();
+
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+            if (config.GetSection("post").Exists())
+            {
+                //this should be post request
+                return await client.PostAsync(url, new FormUrlEncodedContent(CreatePostParameters(config.GetSection("post"), values)));
+
+            }
+            else
+            {
+                return await client.GetAsync(url);
+            }
+
         }
     }
 }
