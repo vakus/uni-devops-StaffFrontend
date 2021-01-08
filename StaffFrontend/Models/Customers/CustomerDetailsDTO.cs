cdfa867da85ef9aa2f3e8b644d90b833e608917e
vakus <sysojewosinskidominik@gmail.com>
2021-01-08 21:23:46 +0000

    Added Reviews to customer details listing

---
+++ StaffFrontend/Models/Customers/CustomerDetailsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffFrontend.Models.Customers
+{
+    public class CustomerDetailsDTO
+    {
+        public Customer Customer { get; set; }
+
+        public List<Review> Reviews { get; set; }
+    }
+}
--- StaffFrontend/Proxies/CustomerProxy/CustomerProxyLocal.cs
+++ StaffFrontend/Proxies/CustomerProxy/CustomerProxyLocal.cs
@@ -1,4 +1,5 @@
 ﻿using StaffFrontend.Models;
+using StaffFrontend.Models.Customers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
